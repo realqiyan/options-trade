2d8e07d609b7fc3f9cc013f47335300a3a898b2a
qiyan <yhb3420@qq.com>
2025-02-19 02:27:51 +0800

    refactor(provider): 优化 K线数据获取逻辑
    -用日 K 线数据替代周 K 线和月 K 线数据
    - 新增 summarySecurityCandlestick 方法汇总日 K 线数据
    -优化代码结构，提高可维护性和可测试性

--- provider/src/main/java/me/dingtou/options/manager/OptionsManager.java
+++ provider/src/main/java/me/dingtou/options/manager/OptionsManager.java
@@ -9,8 +9,10 @@ import me.dingtou.options.model.*;
 import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
@@ -40,30 +42,32 @@ public class OptionsManager {
         // 期权标的价格
         SecurityQuote securityQuote = securityQuoteGateway.quote(security);
 
-        // 周K线
-        SecurityCandlestick weekCandlesticks = candlestickGateway.getCandlesticks(security, CandlestickPeriod.WEEK, 1, CandlestickAdjustType.FORWARD_ADJUST);
-        // 月K线
-        SecurityCandlestick monthCandlesticks = candlestickGateway.getCandlesticks(security, CandlestickPeriod.MONTH, 1, CandlestickAdjustType.FORWARD_ADJUST);
-
 
         // 期权链
         OptionsChain optionsChain = optionsChainGateway.queryOptionsChain(security, optionsStrikeDate.getStrikeTime(), securityQuote.getLastDone());
         optionsChain.setSecurityQuote(securityQuote);
 
-        // K线信息
-        if (!weekCandlesticks.getCandlesticks().isEmpty()) {
-            Candlestick candlestick = weekCandlesticks.getCandlesticks().get(0);
-            // 获取K线波动幅度
-            BigDecimal priceRange = candlestick.getHigh().subtract(candlestick.getLow());
-            optionsChain.setWeekPriceRange(priceRange);
-            optionsChain.setWeekCandlestick(candlestick);
-        }
-        if (!monthCandlesticks.getCandlesticks().isEmpty()) {
-            Candlestick candlestick = monthCandlesticks.getCandlesticks().get(0);
-            // 获取K线波动幅度
-            BigDecimal priceRange = candlestick.getHigh().subtract(candlestick.getLow());
-            optionsChain.setMonthPriceRange(priceRange);
-            optionsChain.setMonthCandlestick(candlestick);
+        // 日K线
+        SecurityCandlestick dayCandlesticks = candlestickGateway.getCandlesticks(security, CandlestickPeriod.DAY, 25, CandlestickAdjustType.FORWARD_ADJUST);
+        if (null != dayCandlesticks && !CollectionUtils.isEmpty(dayCandlesticks.getCandlesticks())) {
+
+            SecurityCandlestick weekCandlesticks = summarySecurityCandlestick(dayCandlesticks, 5);
+            SecurityCandlestick monthCandlesticks = summarySecurityCandlestick(dayCandlesticks, dayCandlesticks.getCandlesticks().size());
+
+            if (null != weekCandlesticks && !CollectionUtils.isEmpty(weekCandlesticks.getCandlesticks())) {
+                Candlestick candlestick = weekCandlesticks.getCandlesticks().get(0);
+                // 获取K线波动幅度
+                BigDecimal priceRange = candlestick.getHigh().subtract(candlestick.getLow());
+                optionsChain.setWeekPriceRange(priceRange);
+                optionsChain.setWeekCandlestick(candlestick);
+            }
+            if (null != monthCandlesticks && !CollectionUtils.isEmpty(monthCandlesticks.getCandlesticks())) {
+                Candlestick candlestick = monthCandlesticks.getCandlesticks().get(0);
+                // 获取K线波动幅度
+                BigDecimal priceRange = candlestick.getHigh().subtract(candlestick.getLow());
+                optionsChain.setMonthPriceRange(priceRange);
+                optionsChain.setMonthCandlestick(candlestick);
+            }
         }
 
         // 恐慌指数
@@ -72,6 +76,61 @@ public class OptionsManager {
         //optionsChain.setVixQuote(vixQuote);
 
         return optionsChain;
+    }
+
+    private SecurityCandlestick summarySecurityCandlestick(SecurityCandlestick dayCandlesticks, int size) {
+        List<Candlestick> candlesticks = dayCandlesticks.getCandlesticks();
+        if (candlesticks.size() < size) {
+            return null;
+        }
+        // timestamp日期由远到近排序
+        candlesticks.sort((o1, o2) -> o2.getTimestamp().compareTo(o1.getTimestamp()));
+        int endIndex = candlesticks.size();
+        List<Candlestick> subList = candlesticks.subList(endIndex - size, endIndex);
+        BigDecimal close = BigDecimal.ZERO;
+        BigDecimal open = BigDecimal.ZERO;
+        BigDecimal low = BigDecimal.ZERO;
+        BigDecimal high = BigDecimal.ZERO;
+        Long volume = 0L;
+        BigDecimal turnover = BigDecimal.ZERO;
+        Long timestamp = 0L;
+        for (int i = 0; i < subList.size(); i++) {
+            Candlestick candlestick = subList.get(i);
+            if (i == 0) {
+                open = candlestick.getOpen();
+                low = candlestick.getLow();
+                high = candlestick.getHigh();
+            }
+
+            if (low.compareTo(candlestick.getLow()) > 0) {
+                low = candlestick.getLow();
+            }
+
+            if (high.compareTo(candlestick.getHigh()) < 0) {
+                high = candlestick.getHigh();
+            }
+            volume += candlestick.getVolume();
+            turnover = turnover.add(candlestick.getTurnover());
+            if (i == subList.size() - 1) {
+                close = candlestick.getClose();
+                timestamp = candlestick.getTimestamp();
+            }
+        }
+
+        SecurityCandlestick summary = new SecurityCandlestick();
+        summary.setSecurity(dayCandlesticks.getSecurity());
+        ArrayList<Candlestick> summaryCandlesticks = new ArrayList<>();
+        Candlestick summaryCandlestick = new Candlestick();
+        summaryCandlestick.setHigh(high);
+        summaryCandlestick.setLow(low);
+        summaryCandlestick.setOpen(open);
+        summaryCandlestick.setClose(close);
+        summaryCandlestick.setVolume(volume);
+        summaryCandlestick.setTurnover(turnover);
+        summaryCandlestick.setTimestamp(timestamp);
+        summaryCandlesticks.add(summaryCandlestick);
+        summary.setCandlesticks(summaryCandlesticks);
+        return summary;
     }
 
 }
