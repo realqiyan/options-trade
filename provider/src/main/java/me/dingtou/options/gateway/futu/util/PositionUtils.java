086f12b40490ab07de0e953e8e838f961f08f299
qiyan <yhb3420@qq.com>
2025-06-18 22:24:11 +0800

    refactor(provider): 优化持仓转换逻辑
    - 在 PositionUtils 类中添加了 convertOwnerPosition 方法的文档注释
    - 优化了代码结构，提高了代码的可读性和可维护性

--- provider/src/main/java/me/dingtou/options/gateway/futu/util/PositionUtils.java
+++ provider/src/main/java/me/dingtou/options/gateway/futu/util/PositionUtils.java
@@ -5,8 +5,18 @@ import java.math.BigDecimal;
 import com.futu.openapi.pb.TrdCommon;
 import me.dingtou.options.model.OwnerPosition;
 
+/**
+ * 持仓转换
+ */
 public class PositionUtils {
 
+    /**
+     * 持仓转换
+     * 
+     * @param position 原始持仓
+     * @param owner    owner
+     * @return 转换后的持仓
+     */
     public static OwnerPosition convertOwnerPosition(TrdCommon.Position position, String owner) {
         if (null == position) {
             return null;
