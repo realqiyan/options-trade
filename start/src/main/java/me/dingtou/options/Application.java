1e4155a39eec31e70e1ffb96cb69fdf1c58f869c
qiyan <yhb3420@qq.com>
2025-07-02 22:07:20 +0800

    feat(start): 添加日志配置
    - 在 Application 类中添加日志对象并输出启动成功日志
    - 新增 logging 配置，设置日志级别、格式和文件输出

--- start/src/main/java/me/dingtou/options/Application.java
+++ start/src/main/java/me/dingtou/options/Application.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 @SpringBootApplication
 @EnableTransactionManagement
@@ -12,8 +14,11 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 @MapperScan("me.dingtou.options.dao")
 public class Application {
 
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+        logger.info("Application started successfully.");
     }
 
 }
