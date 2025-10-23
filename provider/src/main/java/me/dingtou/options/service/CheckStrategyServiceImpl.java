a4d20be40c2ccab2841afcd1ad97b2ddd8114f08
qiyan <yhb3420@qq.com>
2025-10-24 00:20:52 +0800

    docs: 移除提醒内容中的markdown代码块标记

--- provider/src/main/java/me/dingtou/options/service/CheckStrategyServiceImpl.java
+++ provider/src/main/java/me/dingtou/options/service/CheckStrategyServiceImpl.java
@@ -27,12 +27,9 @@ public class CheckStrategyServiceImpl implements CheckStrategyService {
      * 提醒内容
      */
     private static final String REMIND = """
-
-            ```markdown
             * 不要预测，到了指定的点位该rollover就执行。
             * 不要有侥幸心理，一次可能是运气好，不会每次都有好运气。
             * 不要在意短期的时间价值获利，看长线，避免因小失大。
-            ```
 
              """;
 
